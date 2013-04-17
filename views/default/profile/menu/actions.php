c3c28c11303531508cc017dcd8a855ac183bbb1e
Jerome Bakker <jeabakker@coldtrick.com>
2013-04-17 15:34:46 +0200

    first commit to GitHub

---
+++ views/default/profile/menu/actions.php
@@ -0,0 +1,11 @@
+<?php
+
+	/**
+	 * Elgg profile icon hover over: actions
+	 * 
+	 * @package ElggProfile
+	 * 
+	 * @uses $vars['entity'] The user entity. If none specified, the current user is assumed. 
+	 */
+
+?>
